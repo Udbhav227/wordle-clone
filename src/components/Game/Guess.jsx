c5635d368589be342e0538357e133f1b9597678b
Udbhav <udbhav227@gmail.com>
2025-08-03 16:30:16 +0000

    Add answer prop to GuessResults and Guess components for improved guess evaluation

--- src/components/Game/Guess.jsx
+++ src/components/Game/Guess.jsx
@@ -3,13 +3,21 @@ import React from "react";
 import { range } from "../../utils";
 import { checkGuess } from "../../game-helpers";
 
-export default function Guess({ value }) {
+function Cell({ letter, status }) {
+  const className = status ? `cell ${status}` : 'cell';
+  return <span className={className}>{letter}</span>;
+}
+
+export default function Guess({ value, answer }) {
+  const result = checkGuess(value, answer);
   return (
     <p className="guess">
       {range(5).map((num) => (
-        <span className="cell" key={num}>
-          {value ? value[num] : undefined}
-        </span>
+        <Cell
+          letter={result ? result[num].letter : undefined}
+          status={result ? result[num].status : undefined}
+          key={num}
+        ></Cell>
       ))}
     </p>
   );
