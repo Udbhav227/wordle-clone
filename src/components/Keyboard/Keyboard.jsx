4077abf332ae5d4cc9adac42d9b091a971922d0c
Udbhav <udbhav227@gmail.com>
2025-08-19 17:58:08 +0000

    adjust keyboard styling

--- src/components/Keyboard/Keyboard.jsx
+++ src/components/Keyboard/Keyboard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const KEYBOARD_ROWS = [
   ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
   ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
-  ['ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'BACKSPACE'],
+  ['✅', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'BACKSPACE'],
 ];
 
 function getLetterStatuses(checkedGuesses) {
